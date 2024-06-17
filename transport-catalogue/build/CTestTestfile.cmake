524dd779e93d3b00b4e67a2c1db5c9e39e838398
nikolai-gromov <nikolay.gromov.u@yandex.ru>
2024-06-17 04:34:25 +0300

    Editing repo

--- transport-catalogue/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nikolai/practicum/cpp-transport-catalogue/transport-catalogue
-# Build directory: /home/nikolai/practicum/cpp-transport-catalogue/transport-catalogue/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
